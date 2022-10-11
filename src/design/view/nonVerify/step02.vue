23a1896e9ed1ee05140f34c69b854713cf656082
chebin <ckqlss@gmail.com>
2022-10-12 03:03:01 +0800

    slider

--- src/design/view/nonVerify/step02.vue
+++ src/design/view/nonVerify/step02.vue
@@ -4,8 +4,8 @@
   >
     <div class="wrap">
       <div
-        id="carouselExampleCrossfade"
-        class="carousel slide carousel-fade relative"
+        id="carouselExampleCaptions"
+        class="carousel slide relative"
         data-bs-ride="carousel"
       >
         <div
@@ -13,7 +13,7 @@
         >
           <button
             type="button"
-            data-bs-target="#carouselExampleCrossfade"
+            data-bs-target="#carouselExampleCaptions"
             data-bs-slide-to="0"
             class="active"
             aria-current="true"
@@ -21,36 +21,82 @@
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCrossfade"
+            data-bs-target="#carouselExampleCaptions"
             data-bs-slide-to="1"
             aria-label="Slide 2"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCrossfade"
+            data-bs-target="#carouselExampleCaptions"
             data-bs-slide-to="2"
             aria-label="Slide 3"
           ></button>
         </div>
         <div class="carousel-inner relative w-full overflow-hidden">
-          <div class="carousel-item active float-left w-full">
+          <div class="carousel-item active relative float-left w-full">
             <img
-              src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
               class="block w-full"
-              alt="Wild Landscape"
+              alt="..."
             />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
           </div>
-          <div class="carousel-item float-left w-full">
+          <div class="carousel-item relative float-left w-full">
             <img
-              src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
               class="block w-full"
-              alt="Camera"
+              alt="..."
             />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
           </div>
-          <div class="carousel-item float-left w-full">
-            <img src="/images/icon_oh.png" alt="" class="block mx-auto" />
+          <div class="carousel-item relative float-left w-full">
+            <img
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
+              class="block w-full"
+              alt="..."
+            />
+            <div class="carousel-caption hidden md:block absolute text-center">
+              <h5 class="text-xl">Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
           </div>
         </div>
+        <button
+          class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            class="carousel-control-prev-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            class="carousel-control-next-icon inline-block bg-no-repeat"
+            aria-hidden="true"
+          ></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
       <button class="rounded-full btn-lang mx-auto">Preview the service</button>
     </div>
