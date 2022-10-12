9227eec78eedbe2b8544ff1bdec5c676f291a0e5
feat.ruby <feat.ruby@gmail.com>
2022-10-12 21:57:06 +0900

    [feature] nonVerify step02 slide 완료

--- src/design/view/nonVerify/step02.vue
+++ src/design/view/nonVerify/step02.vue
@@ -1,19 +1,19 @@
 <template>
   <div
-    class="h-screen max-w-md mx-auto flex items-center justify-center relative guide-line"
+    class="h-screen max-w-md mx-auto flex justify-center relative guide-line"
   >
     <div class="wrap">
       <div
-        id="carouselExampleCaptions"
+        id="carouselNonverify"
         class="carousel slide relative"
         data-bs-ride="carousel"
       >
         <div
-          class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4"
+          class="carousel-indicators absolute right-0 bottom-0 left-0 flex items-center justify-center p-0 mb-4"
         >
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#carouselNonverify"
             data-bs-slide-to="0"
             class="active"
             aria-current="true"
@@ -21,82 +21,85 @@
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#carouselNonverify"
             data-bs-slide-to="1"
             aria-label="Slide 2"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#carouselNonverify"
             data-bs-slide-to="2"
             aria-label="Slide 3"
           ></button>
         </div>
         <div class="carousel-inner relative w-full overflow-hidden">
           <div class="carousel-item active relative float-left w-full">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-              class="block w-full"
-              alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-              <h5 class="text-xl">First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
+            <div class="carousel-caption">
+              <h5 class="slide-title">
+                Receive your <br />
+                Customized <br />
+                Healthcare plan
+              </h5>
+              <p class="slide-desc mr-auto">
+                Dietary imbalance care plan <br />
+                For the management of <br />
+                hypertension
               </p>
+              <p class="slide-desc ml-auto">
+                Weight control care plan <br />
+                Fore the management of <br />
+                diabetes
+              </p>
+              <div class="slide-icon mx-auto my-4">
+                <img src="/images/icon_oh.png" alt="" />
+              </div>
             </div>
           </div>
           <div class="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-              class="block w-full"
-              alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-              <h5 class="text-xl">Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
+            <div class="carousel-caption">
+              <h5 class="slide-title">
+                Receive your <br />
+                Customized <br />
+                Healthcare 22
+              </h5>
+              <p class="slide-desc mr-auto">
+                Dietary imbalance care plan <br />
+                For the management of <br />
+                hypertension 22
               </p>
+              <p class="slide-desc ml-auto">
+                Weight control care plan <br />
+                Fore the management of <br />
+                diabetes 22
+              </p>
+              <div class="slide-icon mx-auto my-4">
+                <img src="/images/icon_oh.png" alt="" />
+              </div>
             </div>
           </div>
           <div class="carousel-item relative float-left w-full">
-            <img
-              src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-              class="block w-full"
-              alt="..."
-            />
-            <div class="carousel-caption hidden md:block absolute text-center">
-              <h5 class="text-xl">Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
+            <div class="carousel-caption">
+              <h5 class="slide-title">
+                Receive your <br />
+                Customized <br />
+                Healthcare 33
+              </h5>
+              <p class="slide-desc mr-auto">
+                Dietary imbalance care plan <br />
+                For the management of <br />
+                hypertension 33
               </p>
+              <p class="slide-desc ml-auto">
+                Weight control care plan <br />
+                Fore the management of <br />
+                diabetes 33
+              </p>
+              <div class="slide-icon mx-auto my-4">
+                <img src="/images/icon_oh.png" alt="" />
+              </div>
             </div>
           </div>
         </div>
-        <button
-          class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span
-            class="carousel-control-prev-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span
-            class="carousel-control-next-icon inline-block bg-no-repeat"
-            aria-hidden="true"
-          ></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
       <button class="rounded-full btn-lang mx-auto">Preview the service</button>
     </div>
@@ -104,7 +107,7 @@
   </div>
 </template>
 
-<style lang="css">
+<style lang="scss">
 .guide-line {
   border: 1px solid #eee;
 }
@@ -114,6 +117,54 @@
   line-height: 1.13;
   color: #9b9b9b;
   margin: 30px 0 40px;
+}
+#carouselNonverify {
+  .carousel-indicators {
+    button {
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background: #b1d4ff;
+      border: none;
+      &.active {
+        width: 12px;
+        height: 12px;
+        background: #3d95ff;
+      }
+    }
+  }
+  .carousel-inner {
+    .carousel-item {
+      .carousel-caption {
+        display: grid;
+        height: 485px;
+        .slide-title {
+          width: 80%;
+          text-align: left;
+          font-size: 28px;
+          font-weight: bold;
+          line-height: 1.17;
+          color: #3d95ff;
+          padding-bottom: 1rem;
+          border-bottom: 1px solid #3d95ff;
+          display: inline-block;
+        }
+        .slide-desc {
+          font-size: 16px;
+          font-weight: 500;
+          line-height: 1.11;
+          text-align: center;
+          color: #555;
+          padding: 1rem 0;
+          border-bottom: 1px solid #3d95ff;
+          display: inline-block;
+        }
+        .slide-icon {
+          margin: 2rem auto;
+        }
+      }
+    }
+  }
 }
 .btn-lang {
   display: block;
